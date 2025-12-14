e94a3f4b56769979bffc1a46ef831665fff4a84f
arkii1 <tommy.mapp@hotmail.com>
2025-12-14 15:20:17 +0000

    Add Hero page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
+import Hero from "./ui/hero";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        Hello world
-      </main>
-    </div>
+    <Hero/>
   );
 }
---
+++ app/ui/hero.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+export default function Hero()
+{
+    return <>
+        <section className="flex flex-col justify-center items-center h-screen absolute inset-0 h-[1200px] text-center gap-3xs">
+            <h1 className="font-bold">
+                DND CAFE BRISTOL
+            </h1>
+            <p className="text-3xl font-semibold">NOW OPEN</p>
+
+            <Link href="/book" className="cursor-pointer font-semibold shadow-lg mt-8 bg-sky-700 py-4 px-10 rounded-lg">FIND OUT MORE AND BOOK!</Link>
+        </section>
+        <video autoPlay loop muted className="h-[1200px] object-cover pointer-events-none">
+            <source src="/coffee_pour.mp4" type="video/mp4"  />
+            Your browser does not support the video tag.
+        </video>
+    </>
+}
