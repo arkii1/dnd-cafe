a3425950d642edbe2beba07204442734eb0f7698
arkii1 <tommy.mapp@hotmail.com>
2025-12-18 20:51:56 +0000

    Improve responsive design

--- app/ui/gameroom.tsx
+++ app/ui/gameroom.tsx
@@ -2,7 +2,7 @@
 
 export default function GameRoom() {
     return <>
-        <section className="relative h-screen sm:h-[1200px]">
+        <section className="relative h-[60vh] sm:h-[1200px]">
             <div className="absolute flex justify-between items-center insert-0 h-full w-full bg-transparent">
                 <div className="w-full h-3/4 md:w-2/3">
                     <div className="flex flex-col justify-center items-start px-10 md:mx-35 text-start w-full h-full border-stone-100 border-0 rounded-4xl">
@@ -17,7 +17,7 @@ export default function GameRoom() {
                 </div>
             </div>
             
-            <video autoPlay loop muted className="h-screen sm:h-[1200px] object-cover pointer-events-none">
+            <video autoPlay loop muted className="h-[60vh] sm:h-[1200px] object-cover pointer-events-none">
                 <source src="/game_room.mp4" type="video/mp4"  />
                 Your browser does not support the video tag.
             </video>
--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -3,13 +3,13 @@ import Link from "next/link"
 export default function Hero()
 {
     return <>
-        <section className="relative h-screen sm:h-[1200px]">
+        <section className="relative h-[90vh] sm:h-[1200px]">
             <div className="absolute flex flex-col justify-start items-center inset-x-0 text-center sm:gap-2xs top-1/3 mt-15 sm:top-1/4 sm:mt-0">
                 <h1 className="text-3xl font-black sm:tracking-wider">
                     DND CAFE BRISTOL
                 </h1>
                 <p className="text-4xl sm:text-6xl font-bold tracking-widest">NOW OPEN</p>
-                <Link href="/book" className="cursor-pointer font-semibold shadow-lg mt-8 mx-4 bg-sky-700 py-4 px-19 outline-1 outline-sky-900/80 shadow-lg">FIND OUT MORE AND BOOK!</Link>  
+                <Link href="/book" className="cursor-pointer font-semibold shadow-lg mt-8 mx-4 bg-sky-700 py-4 px-8 max-w-[320px] sm:px-19 sm:w-auto outline-1 outline-sky-900/80 shadow-lg">FIND OUT MORE AND BOOK!</Link>  
             </div>
             <video autoPlay loop muted className="h-screen sm:h-[1200px] object-cover pointer-events-none w-full">
                 <source src="/coffee_pour.mp4" type="video/mp4"  />
