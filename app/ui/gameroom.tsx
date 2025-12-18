b6607f053f9a131064de69660ab6ebb9dd5dc666
arkii1 <tommy.mapp@hotmail.com>
2025-12-18 21:29:52 +0000

    Gameroom improvements

--- app/ui/gameroom.tsx
+++ app/ui/gameroom.tsx
@@ -2,7 +2,7 @@
 
 export default function GameRoom() {
     return <>
-        <section className="relative h-[100vh] sm:h-[1200px]">
+        <section className="relative h-[100vh] sm:h-[1200px] bg-slate-100">
             <div className="absolute flex justify-between items-center insert-0 top-0 h-1/2 mx-10">
                 <div className="flex flex-col justify-center items-start text-start gap-3">
                     <h2 className="text-4xl font-extrabold">BOOK NOW</h2>
@@ -11,7 +11,7 @@ export default function GameRoom() {
             </div>
 
             <div className="absolute flex flex-col justify-start items-between py-4 insert-0 h-2/3 top-1/3 mx-10">
-                <div className="flex flex-col justify-start items-start p-4 text-start gap-3 h-full bg-slate-100 text-sky-700 shadow outline-0">
+                <div className="flex flex-col justify-start items-start p-4 text-start gap-3 h-full bg-slate-100 text-sky-700 shadow-lg outline-0">
                     <h3 className="text-2xl font-extrabold">SELECT YOUR PACKAGE & DATE</h3>
                     <p>package here</p>
                     <p>date here</p>
