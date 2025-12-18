a2d662f8bee6b2c1d55cabde8097d79bb16ca08b
arkii1 <tommy.mapp@hotmail.com>
2025-12-18 21:21:40 +0000

    Change mobile responsive design

--- app/ui/gameroom.tsx
+++ app/ui/gameroom.tsx
@@ -2,22 +2,24 @@
 
 export default function GameRoom() {
     return <>
-        <section className="relative h-[60vh] sm:h-[1200px]">
-            <div className="absolute flex justify-between items-center insert-0 h-full w-full bg-transparent">
-                <div className="w-full h-3/4 md:w-2/3">
-                    <div className="flex flex-col justify-center items-start px-10 md:mx-35 text-start w-full h-full border-stone-100 border-0 rounded-4xl">
-                        <h2 className="text-7xl font-bold">BOOK NOW</h2>
-                        <p className="italic text-m">Reserve & pay, and recieve access to over twenty retro consoles, with over 100 games and 80 board games.</p>
-                    </div>
-                </div>
-                <div className="right-0 h-full bg-stone-900 w-1/3 items-center shadow-2xl  hidden md:flex">
-                    <div className="m-8 p-8 w-full h-1/3 rounded-4xl flex-col justify-center items-center border-2 border-stone-100">
-                        Insert calender here
-                    </div>
+        <section className="relative h-[100vh] sm:h-[1200px]">
+            <div className="absolute flex justify-between items-center insert-0 top-0 h-1/2 mx-10">
+                <div className="flex flex-col justify-center items-start text-start gap-3">
+                    <h2 className="text-4xl font-extrabold">BOOK NOW</h2>
+                    <p className="italic text-m">Reserve & pay, and recieve access to a range retro consoles & board games.</p>
                 </div>
             </div>
+
+            <div className="absolute flex flex-col justify-start items-between py-4 insert-0 h-2/3 top-1/3 mx-10">
+                <div className="flex flex-col justify-start items-start p-4 text-start gap-3 h-full bg-slate-100 text-sky-700 shadow outline-0">
+                    <h3 className="text-2xl font-extrabold">SELECT YOUR PACKAGE & DATE</h3>
+                    <p>package here</p>
+                    <p>date here</p>
+                </div>
+                <small className="text-xs font-thin italic ml-auto mt-2 text-slate-100/40">Powered by example</small>
+            </div>
             
-            <video autoPlay loop muted className="h-[60vh] sm:h-[1200px] object-cover pointer-events-none">
+            <video autoPlay loop muted className="h-1/2 w-full sm:h-[1200px] object-cover pointer-events-none">
                 <source src="/game_room.mp4" type="video/mp4"  />
                 Your browser does not support the video tag.
             </video>
